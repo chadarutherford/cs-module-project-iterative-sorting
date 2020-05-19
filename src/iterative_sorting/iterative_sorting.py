0ab41fcc54a1df9dbb0649568bacf37432675347
Chad Rutherford <chad_rutherford@outlook.com>
2020-05-19 15:41:05 -0400

    Implemented CountSort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -39,9 +39,37 @@ def bubble_sort(arr):
     return arr
 
 
-# STRETCH: implement the Count Sort function below
+# requires us to know the "max" value that we'll be sorting
+# the maximum arg was so that  we could specify the max value
+# The total range of data we'll be sorting sits between 0 and maximum
 def count_sort(arr, maximum=-1):
-    # Your code here
+    if len(arr) == 0:
+        return arr
 
+    # if maximum is not given, we will take the max value from the input array
+    if maximum == -1:
+        maximum = max(arr)
 
+    # make a bunch of buckets
+    buckets = [0 for _ in range(maximum + 1)]
+
+    for x in arr:
+        if x < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        buckets[x] += 1
+
+    # we now have a count of every value in the array
+    # loop through the buckets starting at the smallest index
+    j = 0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j += 1
+            buckets[i] -= 1
     return arr
+
+
+array = [1, 2, 3, 4, 6, 3, 4, 2, 4, 3, 1, 0, 6 ,0]
+print(f"Array before sort: {array}")
+count_sort(array)
+print(f"Array after sort: {array}")
